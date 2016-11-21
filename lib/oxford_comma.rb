16e1d5cbc56855d4e27584e33bd43c4293bd59f4
Joel Cowie <joel.a.cowie@gmail.com>
2016-11-21 02:13:00 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,11 @@
 def oxford_comma(array)
-
+  if array.length < 3
+    array.join(" and ")
+  elsif array.length == 3
+    return "#{array[0]}, #{array[1]}, and #{array[2]}"
+  else
+    last_item = array.pop
+    string = array.join(", ")
+    string << ", and #{last_item}"
+  end
 end
